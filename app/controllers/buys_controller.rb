d8cc8d202f7c61bcad1f3629e749229ba6220093
island0702 <126866343+island0702@users.noreply.github.com>
2023-04-09 10:44:04 +0900

    手順3

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,18 +1,19 @@
 class BuysController < ApplicationController
-  before_action :authenticate_user!
-  # before_action :non_purchased_item, only: [:index, :create]
 
   def index
-    @buy_address = BuyAddress.new
     @item = Item.find(params[:item_id])
+    @buy_address = BuyAddress.new
+   
   end
 
   def create
+    
+    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(buy_params)
     if @buy_address.valid?
       pay_item
-      @buy_address.save
-      redirect_to root_path
+       @buy_address.save
+       redirect_to root_path
     else
       render :index
     end
@@ -20,26 +21,20 @@ class BuysController < ApplicationController
 
   private
 
-  # def buy_params
-  #   # この時点では、order_idが不要。またrequire外の情報は参照するため、mergeとする。
-  #   params.require(:order_form).
-  #   permit(:municapality, :address, :area_of_origin_id, :telephone_number, :building_name, :post_code).
-  #   merge(user_id: current_user.id, item_id: 
-  #     params[:item_id], token: params[:token])
-  # end
+  def buy_params
+    params.require(:buy_address).
+    permit(:municapality, :address, :area_of_origin_id, :telephone_number, :building_name, :post_code).
+    merge(user_id: current_user.id, item_id: @item.id)
+  end
 
-  # def pay_item
-  #   Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-  #   Payjp::Charge.create(
-  #     amount: @item.price,        # 商品の値段
-  #     card: buy_params[:token], # カードトークン
-  #     currency: 'jpy'             # 通貨の種類（日本円）
-  #   )
-  # end
-
-#   def non_purchased_item
-#     # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
-#     @item = Item.find(params[:item_id])
-#     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
-#   end
+  def pay_item
+    Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+  
 end
+
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,15 +1,20 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :item, :user, :post_code, :address, :building_name, :building_name, :telephone_number
-
-  validates :prefecture_id, numericality: { other_than: 1 }
+  attr_accessor :item_id, 
+                :user_id,
+                :municapality, 
+                :area_of_origin_id, 
+                :address, 
+                :post_code, 
+                :telephone_number,
+                :building_name
 
   with_options presence: true do
-    validates :item
-    validates :user
+    validates :item_id
+    validates :user_id
+    validates :municapality
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :address
-    validates :building_name
     validates :telephone_number, format: {with: /\A[0-9]{11}\z/ }
     validates :area_of_origin_id
   end
@@ -22,12 +27,12 @@ class BuyAddress
 
     Address.create(
       buy_id: buy.id,
-      municipality: municipality, 
+      municapality: municapality, 
       area_of_origin_id: area_of_origin_id, 
       address: address, 
       post_code: post_code, 
       telephone_number: telephone_number, 
-      building_name: building_name, 
+      building_name: building_name
     )
   end
 end
